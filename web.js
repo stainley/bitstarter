eef6d536915989eb8740b4c325d15772b82c4ee7
Stainley Lebron <saiyamans@gmail.com>
2013-07-08 17:56:02 +0000

    Add read from file index.html

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var fs = requiere('fs');
+
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function readFileSync(fileName){
+  var content = fs.readFileSync(fileName, 'utf-8');
+  return content;
+}
