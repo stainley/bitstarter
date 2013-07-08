0ce9fd2442a2ffe83f38fb5f5934245eb1377385
Stainley Lebron <saiyamans@gmail.com>
2013-07-08 19:30:40 +0000

    Added file index.html and modified web.js

--- web.js
+++ web.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var fs = requiere('fs');
 
+var content = fs.readFileSync("index.html");
 
+function readFileHtml(){
+    var buffer = new Buffer(content, 'utf-8');
+    return buffer.toString();
+}
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(readFileSync('index.html'));
+  response.send(readFileHtml());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-function readFileSync(fileName){
-  var content = fs.readFileSync(fileName, 'utf-8');
-  return content;
-}
